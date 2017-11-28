d49fbbc4a196e7bb8835885f5da18322936b9b3c
raj_mishra <rajkiit116@gmail.com>
2017-11-28 22:16:16 +0530

    ui works fine .have to work on url direction

--- src/main/java/com/javacodeuniversity/web/controller/HomePageController.java
+++ src/main/java/com/javacodeuniversity/web/controller/HomePageController.java
@@ -8,12 +8,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomePageController {
-
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		return "index";
+	}	
+	@RequestMapping(value = "/DataStructure", method = RequestMethod.GET)
+	public String getDataStructureTutorial(ModelMap model) {
+		return "/datastructure/datastructure";
+	}
+	@RequestMapping(value = "/SpringBoot", method = RequestMethod.GET)
+	public String getSpringBootTutorial(ModelMap model) {
+		return "/springboot/springboot";
 	}
 	
+	@RequestMapping(value = "/Java", method = RequestMethod.GET)
+	public String getJavaTutorial(ModelMap model) {
+		return "/java/java";
+	}
+	@RequestMapping(value = "/JavaCodingInterviewQuestion", method = RequestMethod.GET)
+	public String getJavaCodingInterviewQuestion(ModelMap model) {
+		return "/java/java_coding_interview_question/java_coding_interview_question";
+	}
+	
+	@RequestMapping(value = "/JavaInterviewQuestion", method = RequestMethod.GET)
+	public String getJavaInterviewQuestion(ModelMap model) {
+		return "/java/java_interview_question/java_interview_question";
+	}
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String testMethod(ModelMap model) {
 		return "index";
